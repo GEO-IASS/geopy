f48c57343b7e5e90c66f52539dc82203594bf5f0
Claudio Santos <contato@simplologia.com.br>
2017-03-10 15:58:48 -0300

    add test.tomtom module as TomTomTestCase

--- test/geocoders/__init__.py
+++ test/geocoders/__init__.py
@@ -8,13 +8,14 @@ from .dotus import GeocoderDotUSTestCase
 from .geocodefarm import GeocodeFarmTestCase
 from .geonames import GeoNamesTestCase
 from .googlev3 import GoogleV3TestCase
+from .ignfrance import IGNFranceTestCase
+from .mapzen import MapzenTestCase
 from .nominatim import NominatimTestCase
 from .opencage import OpenCageTestCase
 from .openmapquest import OpenMapQuestTestCase
+from .photon import PhotonTestCase
 from .placefinder import YahooPlaceFinderTestCase
 from .smartystreets import LiveAddressTestCase
+from .tomtom import TomTomTestCase
 from .what3words import What3WordsTestCase
 from .yandex import YandexTestCase
-from .ignfrance import IGNFranceTestCase
-from .photon import PhotonTestCase
-from .mapzen import MapzenTestCase
